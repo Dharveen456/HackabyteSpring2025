9542bb0d6424a63fd365ffa5bff170eda9efe584
steveleecode <stevelee_2008@outlook.com>
2025-03-29 15:47:38 -0700

    Added Live View

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import cv2
 
 
 app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+if not camera.isOpened():
+    raise Exception("Could not open video device")
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()  # Capture frame-by-frame
+        if not success:
+            break
+        else:
+            # Convert frame to JPEG format
+            _, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            # Display frame in byte format
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            
+@app.route('/video_feed')
+def video_feed():
+    #sets to a continuously updating dynamic stream with a frame boundary string
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/")
 def index():
